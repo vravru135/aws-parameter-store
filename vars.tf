326e3461b04d6f93af516a210f95bd49206a9110
vravru135 <vravru135@gmail.com>
2024-03-21 18:32:13 +0530

    Adding shell Topics

--- vars.tf
+++ vars.tf
@@ -11,6 +11,6 @@ variable "parameters" {
     ### Passwords.
     # Passwords should never be kept under code, It always been created mostly manually / saperate automation where code is not involved.
     "dev.rds.password"  = { name = "dev.rds.password", type = "SecureString", value = "ExpenseApp1234" },
-    "prod.rds.password" = { name = "prod.rds.password", type = "SecureString", value = "ExpenseApp1234" },,
+    "prod.rds.password" = { name = "prod.rds.password", type = "SecureString", value = "ExpenseApp1234" },
   ]
 }
