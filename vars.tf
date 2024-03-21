09ed497da13b30c92b7e68e313303fe4e22f3c8e
vravru135 <vravru135@gmail.com>
2024-03-21 18:10:07 +0530

    Adding shell Topics

--- vars.tf
+++ vars.tf
@@ -1,9 +1,16 @@
 variable "parameters" {
   default = [
-    { name = "dev.rds.username", type = "String", value = "admin1"},
     { name = "dev.backend.DB_HOST", type = "String", value = "dev-mysql-rds.cbsk880ayzha.us-east-1.rds.amazonaws.com"},
 
-    ### Passwords
+    ## RDS
+    { name = "dev.rds.endpoint", type = "String", value = "dev-mysql-rds.cbsk880ayzha.us-east-1.rds.amazonaws.com"},
+    { name = "dev.rds.username", type = "String", value = "admin1"}
+
+
+
+
+
+   ### Passwords
     # Passwords should never be kept under code, it always been created mostly manually / saperate automation where code is not involved.
     { name = "dev.rds.password" , type = "SecureString", value = "ExpenseApp1234" },
   ]
